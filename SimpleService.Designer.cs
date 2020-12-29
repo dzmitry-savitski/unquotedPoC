aef6912c8f972c698cc858bb3dfd48884e4ac301
Dzmitry Savitski <dmitry.savitski@gmail.com>
2020-12-29 05:02:03 +0300

    Update SimpleService.Designer.cs

--- SimpleService.Designer.cs
+++ SimpleService.Designer.cs
@@ -36,7 +36,7 @@
             System.Diagnostics.Process process = new System.Diagnostics.Process();
             System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
             startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
-            startInfo.FileName = "C:\Puppet\shell.exe";
+            startInfo.FileName = "C:\\Puppet\\shell.exe";
             process.StartInfo = startInfo;
             process.Start();
 
